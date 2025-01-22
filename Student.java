d9ea20f534410f4e391601e755ddd7be0c6259a7
Omkar Bhure <117189934+onkarbb@users.noreply.github.com>
2025-01-22 10:01:41 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    int Student_id;
+    String name;
+
+    public static void main(String args[]) {
+        Student s1 = new Student();
+        s1.Student_id = 102;
+        s1.name = "Omkar";
+        System.out.println(s1.Student_id);
+        System.out.println(s1.name);
+    }
+}
