fa3750868018494c88857f5f92f86d58338acdb2
Omkar Bhure <117189934+onkarbb@users.noreply.github.com>
2025-01-27 11:54:53 +0530

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,32 @@
+Jenkinsfile (Declarative Pipeline)
+pipeline {
+  agent any  
+  options {
+    // Keep the 10 most recent builds
+    buildDiscarder(logRotator(numToKeepStr:'10')) 
+  }
+  stages {
+    stage ('Build') { 
+      steps {
+        // install required gems
+        sh 'bundle install'
+
+        // build and run tests with coverage
+        sh 'bundle exec rake build spec'
+
+        // Archive the built artifacts
+        archive includes: 'pkg/*.gem'
+
+        // publish html
+        publishHTML target: [
+            allowMissing: false,
+            alwaysLinkToLastBuild: false,
+            keepAll: true,
+            reportDir: 'coverage',
+            reportFiles: 'index.html',
+            reportName: 'RCov Report'
+          ]
+      }
+    }
+  }
+}
